fd12771f3eb091e5e58b7d6b78023ee1b12ce6ce
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2024-09-17 07:53:16 -0500

    Update uif.py

--- Parcial1/ui/uif.py
+++ Parcial1/ui/uif.py
@@ -18,9 +18,33 @@ def get_user_data(client):
     size = request_size()
     departament = request_departament()
     client = piapi.contact_api()
-    results = piapi.save_data(piapi.get_data(size, departament, client))
-    return results
+    
+    # Obtener datos de la API
+    raw_results = piapi.get_data(size, departament, client)
+    
+    # Convertir a DataFrame
+    results = piapi.save_data(raw_results)
+    
+    # Exploración inicial de los datos extraídos
+    piapi.initial_exploration(results)
+    
+    # Formatear y procesar los datos
+    formatted_results = piapi.format_data(results)
+    wrangled_results = piapi.data_wrangling(formatted_results)
+    
+    # Identificación de valores faltantes en los datos procesados
+    piapi.missing_values(wrangled_results)
+    
+    # Estadísticas básicas de los datos procesados
+    piapi.basic_statistics(wrangled_results)
+    
+    return wrangled_results
 
 def show_data(data):
     """This function shows the data"""
-    print(piapi.format_data(data))
+    print(data)
+    
+
+
+
+
